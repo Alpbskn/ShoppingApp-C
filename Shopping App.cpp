cc12fbf2635066d1f08b54dc96e02429efc6e3d3
Alpbskn <94756954+Alpbskn@users.noreply.github.com>
2021-11-20 22:39:57 +0300

    Add files via upload

--- Shopping App.cpp
+++ Shopping App.cpp
@@ -2,11 +2,14 @@
  #include <iostream>
 
 int main() {
-	// Her alt baþlýk için islem adýnda integer oluþturdum.1-2-3-4 diye sýraladým ve en geniþ baþlýk olan "islem" için 0 deðerini atadým. Ýleride buraya dönmek istediðimde islem=0 komutunu kullanacaðým.
-	// Hesabýn tutarý uzun bir sayý çýkabileceðinden dolayý long olarak tanýmladým ve en baþta sýfýrdan baþlamasý gerektiðini düþündüðümden 0 deðerini atadým.
+	/* Her alt baþlýk için islem adýnda integer oluþturdum.1-2-3-4 diye sýraladým ve en geniþ baþlýk olan "islem" için 0 deðerini atadým.
+	Ýleride buraya dönmek istediðimde islem=0 komutunu kullanacaðým.
+	Hesabýn tutarý uzun bir sayý çýkabileceðinden dolayý long olarak tanýmladým ve en baþta sýfýrdan baþlamasý gerektiðini düþündüðümden 0 deðerini atadým.*/
 	int islem=0,islem1,islem2,islem3,islem4;
 	long hesap=0,indirimli=0;
-    while(islem==0)
+    
+    //Giriþ ekranýmýzý yazdýralým ve programýn herhangi bir yerinden tekrar buraya dönebilmek için tüm programý while döngüsü içine alalým.
+	while(islem==0)
 	{
     printf("\n         **>> Alp Elektronik <<**      ");
     printf("\n---------------Hosgeldiniz---------------");
@@ -15,7 +18,9 @@ int main() {
     printf("1-Urun ve Fiyat Sorgula\n2-Sepete Urun Ekle\n3-Siparis Tamamla\n4-Sonlandir");
     printf("\nIstenilen islem:");
 	scanf("%d",&islem);   
-        while(islem<=0 || islem>=5)
+     	// Olasýlýklarý deðerlendirmeden önce kullanýcýdan hatalý giriþ yapýp yapmadýðýný denetlemek için bize lazým olan aralýklarýn dýþýný while döngüsüne aldým.
+     	//Bu sayede kullanýcýnýn hatalý giris yaptýðý kadar döngü tekrar edecek ve bir sonraki adýma geçmeyecek. 
+		while(islem<=0 || islem>=5)
         {
         	printf("\nHatali giris yaptiniz. Lutfen tekrar deneyiniz: ");
 	    	scanf("%d",&islem);
@@ -40,7 +45,9 @@ int main() {
 				islem=4;
 			}	
 		}
-		
+		// Ýlk bölümüm olan Ürün ve fiyat sorgulama ekraný için en geniþ deðiþken olan islem'e 1 deðerini atadým.
+		// While döngüsünün içine almamýn sebebi ileride buraya tekrar dönmek istediðimizde bize yardýmcý olmasýdýr.
+		//islem=1; komutunu girdiðimde buraya dönecektir.
 		while(islem==1)	
 		{
 			printf("\n\n******************************************\n");  
@@ -50,12 +57,15 @@ int main() {
 		    printf("******************************************\n");
 		    printf("Seciminiz: ");
 	        scanf("%d",&islem2);
-	        while (0>=islem2 || islem2>=5)
+	        //Önce hatalý seçim yapýp yapmadýðýmýzý denetliyoruz.Doðru cevaplardan birini girene kadar döngü sürmeli.
+		    while (0>=islem2 || islem2>=5)
 			{
 				printf("\nHatali secim yaptiniz.Lutfen tekrar secim yapiniz: ");
 				printf("\nSeciminiz: ");
+				// islem2 yi doðru çýkana kadar kullancýdan tekrar almalýyýz o yüzden döngünün içine bir daha yazýyorum.
 				scanf("%d", &islem2);
 			}
+			//iþlem2 nin ne olduðuna baðlý koþullarýmý aþaðý yazýyorum. Bu sayede kullancý istediði kategoriden fiyat sorgulayabilecek.
 			if (islem2==1)	
 			{
 			    printf("\n------Akilli Telefon------\n");
@@ -88,22 +98,28 @@ int main() {
 		    	printf("\n Bilgisayar-3: 12599 TL");
 			    printf("\n Bilgisayar-4: 16999 TL"); 
 		 	}      
-			for(;;){
+			//For döngüsünün (;;) þu komutu sayesinde ne olursa olsun tüm koþullarýn true sayýlmasýný saðlýyorum.
+			for(;;)
+			{
 	    	printf("\n\nBaska kategoriden urun sorgulamak icin 1'i, ana menuye donmek icin lutfen 2'yi tuslayin: ");
 	    	scanf("%d",&islem3);
-	            while(islem3<=0 || islem3>=3)
+	            //Bir alt baþlýða indiðim için bu kýsýmda islem3 ü kullanýcýdan isteyeceðim.
+	            //Yeniden önce hatalý giris yapýp yapmadýðýmý öðreniyorum ve doðru cevap alana kadar döngünün devam etmesini saðlýyorum.
+				while(islem3<=0 || islem3>=3)
 	            {
-				printf("\nHatali secim yaptiniz.");
-				break;
+					printf("\nHatali secim yaptiniz.");
+					break;
 				}
 				if(islem3==1)
 	            {
-	                islem=1;
+	                //Ürün sorgulama kýsmýný while(islem==1) döngüsü içine aldýðýmdan burada bir önceki menüye yani ürün sorgulama kýsmýna dönmeyi saðlýyorum.
+					islem=1;
 					break;
 			    }
 			    else if(islem3==2)
                 {
-                	islem=0;
+                	//Bütün komutlarý en baþta while(islem==0) döngüsü içine aldýðýmdan burada ana menüye dönmeyi saðlýyorum.
+					islem=0;
                 	break;
 				}
 			}
@@ -111,6 +127,8 @@ int main() {
     	
 		while(islem==2)
 		{
+			//Alt dallarý oluþturmadan önce ana menüyü ve kullanýcýdan ilk aldýðýmýz kod sonrasýnda çýkan ekranlarý oluþturduðum için islem1'i þimdi kullanýyorum.
+			//Üst tarafta bu yüzden islem2'yi kullandým. 
 			islem1=0;
 			printf("\n\n******************************************\n");
 	    	printf("Sepete Urun Ekleme Ekrani\n\n");
@@ -118,19 +136,25 @@ int main() {
 	        printf("1-Akilli Telefon\n2-Akilli Saat\n3-Kablosuz Kulaklik\n4-Bilgisayar\n");
 	     	printf("******************************************\n");	
 	     	printf("Seciminiz: ");
-	     	scanf("%d",&islem2);
-     		while (0>=islem2 || islem2>4)
+            //Deðiþken fazlalýðý olmamasý açýsýndan islem2 ve 3 'ü burada da kullandým.
+            //Kullanýcý bu menüyü tuþladýðýnda islem=1,3,4'teki islem2'ler okunmayacaðý için bu kýsýmda da islem2'yi rahatlýkla kullanabiliyorum.
+			scanf("%d",&islem2);
+			while (0>=islem2 || islem2>4)
 			{
 				printf("\nHatali secim yaptiniz.Lutfen tekrar secim yapiniz: ");
 				printf("\nSeciminiz: ");
 				scanf("%d", &islem2);
 			}
+			//Doðru cevap aldýktan sonra cevabýmýn koþullarýný yazýyorum.
+			//Bu sayede kullanýcý istediði kategoriden ürünü sepetine ekleyebilecek.
 			if(islem2==1)
 		    {
 	            printf("Lutfen sepetinize Akilli Telefon kategorisinden hangi urunu eklemek istediginizi secin.");
 	            printf("Seciminiz: ");
 	            scanf("%d",&islem3);
-	            while (0>=islem3 || islem3>4)
+	            //Kategori içinde de ürün seçimi yapmamýz gerektiðinden kullancýdan tekrar veri almam gerekiyor.
+	            //Bu bir baþka alt baþlýk olduðu için islem3 deðiþkennini kullanýyorum.
+				while (0>=islem3 || islem3>4)
 				{
 					printf("\nHatali secim yaptiniz.Lutfen tekrar secim yapiniz: ");
 					printf("\nSeciminiz: ");
@@ -164,6 +188,7 @@ int main() {
 				 	scanf("%d",&islem4);
 				 	hesap+=islem3*2149;
 				} 
+				
 				printf("\nIstediginiz urun sepetinize basariyla eklendi.");
 				printf("\n\nBaska kategoriden urun eklemek icin 1'e, ana menuye donmek icin lutfen 2'yi tuslayin: ");
 				scanf("%d",&islem4);
@@ -350,19 +375,24 @@ int main() {
 		    		}
 			}				
 		}	
-		
+		//3. kýsýma geldik.
+		// Bu kýsýmda kullanýcýnýn sepetine eklediði ürünlerin fiyatlarý ekrana yazdýrýlacak.
+		// Sizin gibi özel müþterilimiz için indirim de tanýmladým.Çýkan tutardaki birler basamaðýný sýfýrlýyorum.
+		//Çýkan ekranda hem normal tutar hem de indirimli tutarý görebileceksiniz.
 		while(islem==3)
 		{
 		    printf("\n\n******************************************\n");
 	     	printf("Siparisin Tamamlama Ekrani");
 	     	indirimli=hesap-hesap%10;
 	     	printf("\nHesabinizin tutari %d TL",hesap);
-	     	printf("\nHesabinizin indirimli tutari %d",indirimli);
+	     	printf("\nHesabinizin indirimli tutari %d TL",indirimli);
 		    printf("\n******************************************\n");
 		    printf("Alisverise devam etmek icin 1'i,siparisinizi sonlandirmak icin 2'yi tuslayin.\n");
 		    printf("Seciminiz: ");
 		    scanf("%d",&islem2);
-		    while(0==islem2 || islem2>2)
+		    //Alýþveriþe devam etmek isteyenleri ana menüye yani islem=0 a yönlendiriyorum.
+		    //Alýþveriþi bitirmek isteyenleri ise son kýsým olan islem=4 e yönlendiriyorum.
+			while(0==islem2 || islem2>2)
 			{
 				printf("Hatali secim yaptiniz.Lutfen tekrar deneyiniz.");
 				printf("Seciminiz: ");
@@ -387,7 +417,8 @@ int main() {
 	        printf("Evet ise 1'i, Hayir ise 2'yi tuslayin.\n");
 	        printf("Seciminiz:");
 	    	scanf("%d",&islem2);
-	    	while(islem2<1 || islem2>2)
+	    	//Bu kýsýmda gerçekten programý bitirmek isteyenleri ödeme aþamasýna hayýr diyenleri de islem=0 a yönlendiriyorum.
+			while(islem2<1 || islem2>2)
 	    	{
 	    		printf("Hatali secim yaptiniz.Lutfen tekrar deneyiniz.");
 	    		printf("Seciminiz: ");
